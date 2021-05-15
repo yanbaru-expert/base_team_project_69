21385e8fb34dd3f9dbc3b31a5c6a4b5dbe5099e8
damari <motokin128@gmail.com>
2021-05-15 12:09:05 +0900

    新規投稿機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,17 @@
 class TasksController < ApplicationController
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = User.find(params[:id])
+    task.create(task_params)
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
 end
