c4fb0b54a92d50b0261171a3dc090e5577b295c5
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-17 23:22:53 +0900

    一覧表示機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,10 +5,15 @@ class MessagesController < ApplicationController
 
   def create
     Message.create(message_params)
+    redirect_to action: :index
+  end
+
+  def index
+    @messages = Message.all
   end
 
   private
   def message_params
-    params.require(:message).permit(:name, :age)
+    params.require(:message).permit(:title, :content)
   end
 end
